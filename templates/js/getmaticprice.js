f9116cb19a99472eb85a343e6a94b70e20e756a3
Najia-afk <79993633+Najia-afk@users.noreply.github.com>
2022-12-10 21:20:21 +0100

    Update getmaticprice.js

--- templates/js/getmaticprice.js
+++ templates/js/getmaticprice.js
@@ -16,7 +16,7 @@ function getMaticPrice() {
     })
     .then(function(data) {
       // Check if the response contains an error
-      if (data === undefined || data.error === undefined) { // Add a check for undefined data
+      if (data.error === undefined) { // Add a check for undefined data
         // Get the MATIC price from the response
         var maticbtc = data.maticbtc;
         var maticusd = data.maticusd;
