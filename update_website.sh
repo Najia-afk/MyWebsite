b2fd38379d328e2d4ef11babb4431460c6332125
Najia-afk <79993633+Najia-afk@users.noreply.github.com>
2022-12-13 15:08:33 +0100

    Update update_website.sh

--- update_website.sh
+++ update_website.sh
@@ -1,1 +1,21 @@
+# Clone the Git repository
 git clone https://github.com/Najia-afk/MyWebsite.git
+
+# Set the paths to the files you want to import
+nginx_conf_path="MyWebsite/nginx.conf"
+httpd_conf_path="MyWebsite/httpd.conf"
+
+# Set the path to the destination directory
+dest_dir="/var/www/html/MyWebsite/Config"
+
+# Copy the files to the destination directory
+sudo cp "$nginx_conf_path" "$dest_dir"
+sudo cp "$httpd_conf_path" "$dest_dir"
+
+# Set the paths to the target configuration files
+nginx_conf_target="/etc/nginx/nginx.conf"
+httpd_conf_target="/etc/httpd/conf/httpd.conf"
+
+# Replace the existing configuration files with the imported ones
+sudo cp "$dest_dir/nginx.conf" "$nginx_conf_target"
+sudo cp "$dest_dir/httpd.conf" "$httpd_conf_target"
